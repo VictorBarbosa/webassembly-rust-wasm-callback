99987efa18a7f374722ae27f2d1931d2a2f5a2f3
VictorBarbosa <victor.hugo.p.barbosa@hotmail.com>
2022-08-30 15:36:11 -0400

    added assets

---
+++ app-wasm/src/assets/rust_wasm_callback.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} count
+* @param {Function} callback
+*/
+export function callback_ex(count: number, callback: Function): void;
+/**
+* This sample shows how you can passa a counter and receive call back or a error message
+* if the callback funtion is empty
+* @param {number} count
+* @param {Function | undefined} callback
+*/
+export function callback_ex_with_option(count: number, callback?: Function): void;
---
+++ app-wasm/src/assets/rust_wasm_callback_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function callback_ex(a: number, b: number): void;
+export function callback_ex_with_option(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
